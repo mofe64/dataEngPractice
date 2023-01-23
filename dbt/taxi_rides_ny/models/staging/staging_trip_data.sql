c8cd0a5c8f378333128c8e56dcf27b0f1b2351a7
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2023-01-23 02:07:15 +0000

    udpated staging file

--- dbt/taxi_rides_ny/models/staging/staging_trip_data.sql
+++ dbt/taxi_rides_ny/models/staging/staging_trip_data.sql
@@ -1,5 +1,18 @@
 {{ config(materialized='view') }}
 -- ideally we should be selecting only the fields we want not all
-select *,  {{ get_payment_type_description('payment_type') }} as payment_type_description
+select *,
+{{ dbt_utils.surrogate_key(['tpep_pickup_datetime']) }} as tripid, -- this macro is from the dbt_utils dep we installed, will ally md5 hash fn to params, concat the results to one string and return
+{{ get_payment_type_description('payment_type') }} as payment_type_description
 from {{source('staging', 'yellow_taxi_trips')}}
-limit 100
+-- we give the is_test_run var a default value of true
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
+-- if we want to override this var when running model we can do so via the cmd as follows
+-- dbt build --m <model.sql> --var 'is_test_run: false'
+-- or
+-- dbt run --select model_file_name --var 'is_test_run: false'
+-- or 
+-- dbt run --var 'is_test_run: false'
